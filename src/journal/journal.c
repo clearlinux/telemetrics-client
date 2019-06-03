ec9c9911f6ad93a8935905125feb8b707b0065ab
California Sullivan <california.l.sullivan@intel.com>
2019-06-03 11:03:05 -0700

    journal.c: reduce severity of message when failing to opening old records
    The default configuration for telemetry is to not store records that
    were successfully sent. This would result in a multitude of errors when
    calling the journal program with --include_record, as the records no
    longer existed to open.
    
    Signed-off-by: California Sullivan <california.l.sullivan@intel.com>

--- src/journal/journal.c
+++ src/journal/journal.c
@@ -417,7 +417,7 @@ static void print_record(char *record_id)
 
         recordfp = fopen(filepath, "r");
         if (!recordfp) {
-                telem_perror("Error when opening a record to print");
+                telem_log(LOG_INFO, "Could not open record %s: %s\n", record_id, strerror(errno));
                 return;
         }
 
